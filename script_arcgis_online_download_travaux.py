661fab906b85535ccb538728c17a53908e3f88fe
Eva Useros <79694729+Evusma@users.noreply.github.com>
2023-05-31 15:44:11 +0200

    Add files via upload

---
+++ script_arcgis_online_download_travaux.py
@@ -0,0 +1,30 @@
+#####################################################################
+# Script for downloading data from ArcGIS Online                    #
+#####################################################################
+
+
+from arcgis.gis import GIS
+from datetime import date
+
+today = date.today()
+
+def main():
+    # connection to arcgis online
+    gis = GIS(username = "username", password = "password")
+    
+    # accesing the data
+    item_download = arcgis.gis.Item(gis, itemid = '78d23333b02c4c08a3048c379dfe5a98')
+
+    # export a shapefile and download the data from arcgis online
+    # allowed formats: 'Shapefile','CSV', 'File Geodatabase', 'Feature Collection', 'GeoJson', 'Scene Package', 'KML', 'Excel', 'geoPackage', or 'Vector Tile Package'
+    item_export = item_download.export(title = f" download{today}", export_format = 'Shapefile') 
+    item_export.download(save_path = r'C:\xxx\xxx\xxx\xxx')
+    
+    # delete the shapefile from arcgis online
+    item_export.delete(force = True)
+    
+
+if __name__ == "__main__":
+    main()
+    
+    
