e719e35432d028f513cb43a1eaf50c242c2451be
Eva Useros <79694729+Evusma@users.noreply.github.com>
2023-06-01 07:37:58 +0200

    Update script_arcgis_online_download_travaux.py

--- script_arcgis_online_download_travaux.py
+++ script_arcgis_online_download_travaux.py
@@ -10,21 +10,21 @@ today = date.today()
 
 def main():
     # connection to arcgis online
-    gis = GIS(username = "username", password = "password")
+    gis = GIS(username="username", password="password")
     
     # accesing the data
-    item_download = arcgis.gis.Item(gis, itemid = '78d23333b02c4c08a3048c379dfe5a98')
+    item_download = arcgis.gis.Item(gis, itemid='78d23333b02c4c08a3048c379dfe5a98')
 
     # export a shapefile and download the data from arcgis online
     # allowed formats: 'Shapefile','CSV', 'File Geodatabase', 'Feature Collection', 'GeoJson', 'Scene Package', 'KML', 'Excel', 'geoPackage', or 'Vector Tile Package'
-    item_export = item_download.export(title = f" download{today}", export_format = 'Shapefile') 
-    item_export.download(save_path = r'C:\xxx\xxx\xxx\xxx')
+    item_export = item_download.export(title=f" download{today}", export_format='Shapefile') 
+    item_export.download(save_path=r'C:\xxx\xxx\xxx\xxx')
     
     # delete the shapefile from arcgis online
-    item_export.delete(force = True)
+    item_export.delete(force=True)
     
     # delete the data of arcgis online
-    gis.content.get('78d23333b02c4c08a3048c379dfe5a98').layers[0].delete_features(where = "globalid <> ''")
+    gis.content.get('78d23333b02c4c08a3048c379dfe5a98').layers[0].delete_features(where="globalid <> ''")
     
 
 if __name__ == "__main__":
